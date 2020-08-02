3fb1979c52d665355d4ad7284854e4b76e24ebd9
ChrisFizik <61772064+chrisfizik@users.noreply.github.com>
2020-08-02 18:03:08 -0400

    set datetime filters to Eastern zone from Luxon docs

--- .eleventy.js
+++ .eleventy.js
@@ -16,12 +16,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: 'America/New_York'}).toFormat("dd LLL yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    return DateTime.fromJSDate(dateObj, {zone: 'America/New_York'}).toFormat('yyyy-LL-dd');
   });
 
   // Get the first `n` elements of a collection.
